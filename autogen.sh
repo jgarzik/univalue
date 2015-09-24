e450ac495dc4e3f9807c732d6deb03f2e8727908
Cory Fields <cory-nospam-@coryfields.com>
2015-09-24 14:13:57 -0400

    build: use glibtoolize if possible
    Needed on some BSDs

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,8 @@
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
+if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
 autoreconf --install --force
